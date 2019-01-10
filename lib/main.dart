3d9f87957d6f01158d9114efae2d2175b57a81ce
Avi Cohen <cohena100@gmail.com>
2019-01-10 16:56:08 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:rxdart/rxdart.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Welcome!'), centerTitle: true),
+        body: Center(
+          child: CurrentTime(),
+        ),
+      ),
+    );
+  }
+}
+
+class CurrentTime extends StatefulWidget {
+  @override
+  CurrentTimeState createState() => new CurrentTimeState();
+}
+
+class CurrentTimeState extends State<CurrentTime> {
+  final timer = Observable.periodic(Duration(seconds: 1), (i) => i);
+
+  @override
+  void initState() {
+    super.initState();
+    timer.listen((i) => this.setState(() {}));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(DateFormat.jms().format(new DateTime.now())),
+        FlatButton(
+            onPressed: () {
+              setState(() {});
+            },
+            child: Text("hmmm")),
+      ],
+    );
+  }
+}
