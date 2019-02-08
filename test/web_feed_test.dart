a736bab69546188518c9dfcc894bc2570d606ac1
cohena100 <cohena100@gmail.com>
2019-02-08 17:50:03 +0200

    cleanup

--- test/web_feed_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:test/test.dart';
-//import 'package:myapp/src/feed_sample.dart';
-
-void main() {
-  test('first test', () async {
-//    final rssFeed = await getFeed();
-//    expect(rssFeed.title, 'Reuters: Business News');
-  });
-}
